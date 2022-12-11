61348acb5631a802461d17d0aa257382556593b1
bitoller <bi_toller@hotmail.com>
2022-12-11 12:01:10 -0300

    initial commit

---
+++ src/M1-SPRINT-4/S4-07-M1.js
@@ -0,0 +1,29 @@
+/*1- Faça um programa que peça um valor entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido e continue pedindo
+até que o usuário informe um valor válido.*/
+
+
+
+/*2- Faça um programa que leia um nome de usuário e a sua senha e
+não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro
+e voltando a pedir as informações.*/
+
+
+
+/*3- Faça um programa que enquanto a entrada for números inteiros positivos,
+menores que 10, continue somando. Quando um valor negativo ou maior que 10 for
+informado, pare a execução, alerte a soma e a média.*/
+
+
+
+/*4- Faça um programa que registre votos. Em uma eleição para o grêmio estudantil
+existem apenas 3 candidatos, o candidato a, o candidato b e o candidato c.
+Solicite ao usuário que entre com a letra correspondente a um candidato e
+incremente em +1 o número de votos para o candidato. ⁠Caso o usuário entre com
+um valor que não seja correspondente a um candidato,
+alerte: "Candidato não encontrado".
+Ao final de cada votação alerte a mensagem: "Voto registrado com sucesso".
+Para encerrar a votação é necessário que entre com uma flag (variável de controle)
+de parada: use o x para isso.
+Quando o valor x for inserido alerte o número de votos que cada candidato teve.*/
+
