081fdf3c0d9a18c828e778805b59333c7c256380
bitoller <bi_toller@hotmail.com>
2022-12-14 13:44:31 -0300

    initial commit

---
+++ src/M1-SPRINT-4/S4-19-M1.js
@@ -0,0 +1,17 @@
+function snapCrackle(maxValue) {
+    let string = "";
+    for (let i = 1; i <= maxValue; i++) {
+      if (i % 2 != 0 && i % 5 == 0) {
+        string += "SnapCrackle, ";
+      } else if (i % 2 != 0) {
+        string += "Snap, ";
+      } else if (i % 5 == 0) {
+        string += "Crackle, ";
+      } else {
+        string += `${i}, `;
+      }
+    }
+    return string;
+  }
+  console.log(snapCrackle(12));
+  
