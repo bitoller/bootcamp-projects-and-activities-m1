5da7bb7a997377a7ad5b6636d471bd20642b1647
bitoller <bi_toller@hotmail.com>
2022-12-15 14:11:39 -0300

    solved exercises 1 and 2

--- src/M1-SPRINT-4/S4-08-M1.js
+++ src/M1-SPRINT-4/S4-08-M1.js
@@ -1,32 +1,44 @@
 /*1- Desenvolva uma função chamada verticalText, que receberá um parâmetro:
 1. String, que será uma palavra.
 Deverá imprimir a palavra usando o console.log de forma vertical.*/
-
-
-
+/*
+function verticalText(string) {
+  let i = 0;
+  while (i < string.length) {
+    console.log(string[i]);
+    i++;
+  }
+}
+verticalText("Frontend");
+*/
 /*2- Desenvolva uma função chamada incrementText, que receberá um parâmetro:
 1. String, que será uma palavra.
 e deverá imprimir a palavra usando o console.log de forma que ela vai se
 incrementando, começando somente pela primeira letra, depois a primeira e a
 segunda e assim sucessivamente conforme exemplo.*/
-
-
-
+/*
+function incrementText(string) {
+  let i = 0;
+  let word = "";
+  while (i < string.length) {
+    word += string[i];
+    console.log(word);
+    i++;
+  }
+}
+incrementText("Backend");
+*/
 /*3- Desenvolva uma função chamada incrementTextBackwards, que receberá um parâmetro:
 1. String, que será uma palavra.
 Deverá imprimir a palavra usando o console.log de forma que ela vai se
 incrementando ao contrário, começando somente pela última letra,
 depois a última e a penúltimo e assim sucessivamente conforme exemplo.*/
 
-
-
 /*4- Desenvolva uma função chamada decrementText, que receberá um parâmetro:
 1. String, que será uma palavra.
 ​Deverá imprimir a palavra usando o console.log de forma que ela vai se decrementando,
 começando com a palavra inteira, depois removendo a última,
 a penúltima e assim sucessivamente conforme exemplo.*/
-
-
 
 /*5- Desenvolva uma função chamada incrementTextMiddle, que receberá um parâmetro:
 1. String, que será uma palavra.
@@ -35,16 +47,12 @@ incrementando começando somente pela letra do meio, depois a letra da direita e
 a da esquerda e assim sucessivamente conforme o exemplo. _Caso a palavra tenha
 uma quantidade par de letras então deve retornar "Palavra inválida"_.*/
 
-
-
 /*6- Desenvolva uma função chamada padEnd, que receberá três parâmetros:
 1. String, que será uma palavra.
 2. Number, a quantidade da repetição do caractere.
 3. String, o caractere de repetição.
 Sua função deverá retornar a string preenchida com o caractere de acordo com
 o comprimento passado por parâmetro.*/
-
-
 
 /*7- Desenvolva uma função chamada padStart, que receberá três parâmetros:
 1. String, que é a palavra
@@ -53,8 +61,6 @@ o comprimento passado por parâmetro.*/
 Sua função deverá retornar a string preenchida no início como elemento
 conforme o comprimento passado por parâmetro.*/
 
-
-
 /*8- Desenvolva uma função chamada subString, que receberá três parâmetros:
 1. String, a palavra
 2. Number, a posição inicial
@@ -62,15 +68,11 @@ conforme o comprimento passado por parâmetro.*/
 Sua função deverá retornar parte da string **entre** o início e o fim dos
 indices passadas por parâmetro (não inclui os caracteres dos indices).*/
 
-
-
 /*9- Desenvolva uma função chamada stringRepeater, que receberá dois parâmetros:
 1. String, a palavra
 2. Number, a quantidade de repetição
 Sua função deverá retornar uma nova string que contem o número especificado
 de cópias concatenadas da palavra passada por parâmetro.*/
-
-
 
 /*10- Desenvolva uma função chamada stringCentralizer, que receberá três parâmetros:​
 1. String, que é a palavra
@@ -79,4 +81,3 @@ de cópias concatenadas da palavra passada por parâmetro.*/
 Sua função deverá retornar uma nova string que contem a palavra passada
 por parâmetro centralizada entre os elementos de repetição com um espaço
 em branco nas laterais e a quantidade exata passada por parâmetro.*/
-
