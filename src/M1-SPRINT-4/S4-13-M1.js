250f93d5674dd8ac7e064e96b95c1ba2b9bd053c
bitoller <bi_toller@hotmail.com>
2022-12-15 19:35:58 -0300

    solved

--- src/M1-SPRINT-4/S4-13-M1.js
+++ src/M1-SPRINT-4/S4-13-M1.js
@@ -1,19 +1,19 @@
 /*1- Escreva uma função que recebe um valor do tipo string e com no mínimo 5 caracteres.
 Insira o valor recebido em um array e retorne o array gerado.*/
-/*
+
 function stringValue(word) {
   let myArray = [word];
   return myArray;
 }
 let toShow = stringValue("Bianca");
 console.log(toShow);
-*/
+
 /*2- Escreva uma função que recebe um valor do tipo number.
 Use o valor recebido para acessar uma posição do seguinte array:
 let arr = ["Hermanoteu", "Olonéia", "Migalatéia", "Godart"]⁠
 Caso a posição exista, retorne o nome que se encontra nela.
 ⁠Caso não seja acessado nenhuma posição com o valor recebido, ⁠alerte: "Valor não encontrado".*/
-/*
+
 function numberValue(number) {
   let myArray = ["Hermanoteu", "Olonéia", "Migalatéia", "Godart"];
   if (number >= 0 && number <= 3) {
@@ -24,14 +24,14 @@ function numberValue(number) {
 }
 let toShow2 = numberValue(4);
 console.log(toShow2);
-*/
+
 /*3- Crie um array de números, contendo os valores de 1 a 10.
 Crie uma função que recebe um número inteiro.
 Acesse a posição do array de números usando o valor recebido.
 Teste se o valor contido na posição é par ou ímpar.
 Caso seja par retorne: "O valor encontrado nesta posição é par",
 Caso seja ímpar retorne: "O valor encontrado nesta posição é ímpar".*/
-/*
+
 let wholeNumber = parseInt(prompt("Digite um número inteiro"));
 function numberValue2(number) {
   let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -44,7 +44,7 @@ function numberValue2(number) {
 }
 let toShow3 = numberValue2(wholeNumber);
 console.log(toShow3);
-*/
+
 /*4- Dado o array de nomes:
 ⁠let nomes = ["Pedro", "Rafael", "José"]⁠
 Crie uma função que recebe a lista de nomes como parâmetro.
@@ -53,7 +53,7 @@ de caracteres e retornar o nome.
 Teste cada posição usando estrutura de repetição,
 a menos que a lista tenha apenas um valor inserido.
 Use este formato: "O nome x é o maior da lista";*/
-/* 
+
 function list(nameList) {
   let wordSize = nameList[0].length;
   let biggerName = nameList[0];
@@ -68,7 +68,7 @@ function list(nameList) {
 let nameList = ["Pedro", "Rafael", "José"];
 let bigName = list(nameList);
 console.log(`O nome ${bigName} é o maior da lista`);
-*/
+
 /*5- Dado o array de números como exemplo:
 ⁠numeros = [1,4,6,9,11,8]
 Escreva uma função que recebe a lista de números como parâmetro.
@@ -77,7 +77,7 @@ da lista e armazenar o valor da soma em uma variável.
 Use a variável para testar se o valor da soma é um múltiplo de 2.
 Se sim, retorne: "A soma é um múltiplo de 2".
 Caso contrário: "A soma não é um múltiplo de 2"*/
-/* 
+
 function numberList(numbers) {
   let sum = 0;
   if (numbers.length >= 2) {
@@ -92,7 +92,7 @@ function numberList(numbers) {
 let numberArray = [1, 4, 6, 9, 11, 8];
 let multiple = numberList(numberArray);
 console.log(multiple);
-*/
+
 /*6- Dado o array de nomes como exemplo:
 ⁠let nomes = ["Pedro", "Rafael", "José", "Carla", "Maria", "Josef", "Raquel"]⁠
 Escreva uma função que recebe como parâmetros a lista de nomes, um nome (string)
@@ -103,7 +103,7 @@ Teste se o nome recebido pela função como parâmetro é o mesmo que o armazena
 Lembre-se de normalizar os dados antes de testar.
 Caso os valores sejam iguais, retorne: "Acertei".
 Caso não sejam, retorne: "Não é quem eu pensava"*/
-/* 
+
 function list(nameList, name, index) {
   let namePosition = nameList[index];
   if (name.toLowerCase() == namePosition.toLowerCase()) {
@@ -115,7 +115,7 @@ function list(nameList, name, index) {
 let names = ["Pedro", "Rafael", "José", "Carla", "Maria", "Josef", "Raquel"];
 let isCorrectName = list(names, "Guilherme", 3);
 console.log(isCorrectName);
-*/
+
 /*7- Dado dois arrays numéricos como exemplo.
 ⁠let numeros = [1,2,3,4,5,6,10]
 ⁠let outrosNumeros = [5,7,9,4,2,3,9]
