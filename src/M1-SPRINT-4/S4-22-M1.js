ccd66a32ea3dbc0b3cc3a8d9af6a65f167964104
bitoller <bi_toller@hotmail.com>
2022-12-16 11:55:01 -0300

    solved a few exercises

--- src/M1-SPRINT-4/S4-22-M1.js
+++ src/M1-SPRINT-4/S4-22-M1.js
@@ -8,7 +8,17 @@ Cidade
 Estado
 Logradouro
 Crie o objeto Pessoa que tenha todas propriedades citadas.*/
-
+/* 
+let personList = {
+  name: "Elizabeth Trumbull",
+  yearOfBirth: 1991,
+  cpf: 51692345434,
+  city: "Atlanta",
+  state: "GA",
+  address: "611 Elk Creek Road",
+};
+console.log(personList)
+*/
 /*2- Modelando Escola.
 Dado as propriedades:
 Nome
@@ -19,7 +29,18 @@ Estado
 Logradouro
 Curso
 Crie o objeto Escola que tenha todas propriedades citadas.*/
-
+/* 
+let schoolList = {
+  name: "Kenzie Academy",
+  cnpj: 51692345434,
+  fieldArea: "T.I.",
+  city: "Curitiba",
+  state: "Paraná",
+  address: "Rua Ildo Olinski",
+  course: "Fullstack",
+};
+console.log(schoolList);
+*/
 /*3- Modelando Curso.
 Dado as propriedades:
 Nome
@@ -27,7 +48,15 @@ Duração em anos
 Turma
 Módulos
 Crie o objeto Curso que tenha todas propriedades citadas.*/
-
+/* 
+let courseList = {
+  name: "Programação Fullstack",
+  durationInYears: 1,
+  class: "T16",
+  modules: 6,
+};
+console.log(courseList);
+*/
 /*4- Modelando Endereço.
 Dado as propriedades:
 Cidade
@@ -35,7 +64,15 @@ Estado
 Logradouro
 Cep
 Crie o objeto Endereço que tenha todas propriedades citadas.*/
-
+/* 
+let addressList = {
+  city: "Curitiba",
+  state: "Paraná",
+  address: "Rua Francisco Moro",
+  zipCode: 81070340,
+};
+console.log(addressList);
+*/
 //2 ª Parte: Manipulação
 /*Dado o objeto:
 const figure = {  
@@ -47,20 +84,59 @@ const figure = {
     }*/
 /*1- Escreva uma função chamada returnName. A função, quando chamada,
 deve retornar o nome do objeto.*/
-
+/* 
+function returnName() {
+  const figure = {
+    name: "Carl",
+    classes: ["Warrior"],
+    leader_trend: true,
+    power: 6,
+    allied: true,
+  };
+  return figure.name;
+}
+returnName();
+console.log(returnName());
+*/
 /*2- Escreva uma função chamada verifyClasses. A função quando chamada,
 deve retornar de qual classe é o objeto figure.
 Perceba que a propriedade é uma lista - quando a lista tiver apenas um elemento,
 retorne somente o valor do elemento, e quando a lista tiver mais que um elemento,
 retorne toda a lista.*/
-
+/* 
+function verifyClasses() {
+  const figure = {
+    name: "Carl",
+    classes: ["Warrior"],
+    leader_trend: true,
+    power: 6,
+    allied: true,
+  };
+  return figure.classes[0];
+}
+verifyClasses();
+console.log(verifyClasses());
+*/
 /*3- Escreva uma função chamada realPower. A função quando chamada deve retornar
 a força real (power) do objeto figure.
 Caso o objeto tenha a propriedade leader_trend definida como true,
 o valor da propriedade power é elevada ao quadrado.
 Retorne o valor da força do objeto.*/
-
-/*4- Escreva uma função chamada inserNewClass. A função deve receber uma string como parâmetro.
+/* 
+function realPower() {
+  const figure = {
+    name: "Carl",
+    classes: ["Warrior"],
+    leader_trend: true,
+    power: 6,
+    allied: true,
+  };
+  return figure.power * figure.power;
+}
+realPower();
+console.log(realPower());
+*/
+/*4- *****Escreva uma função chamada inserNewClass. A função deve receber uma string como parâmetro.
 A função realiza a inserção de mais uma classe na lista de classes do objeto.
 Cada objeto pode obter somente 3 classes.
 Antes de inserir uma classe, verifique se o tamanho da lista de classes é maior que 3.
@@ -73,6 +149,35 @@ Se sim, retorne: "O personagem já pertence a classe x"
 Caso seja diferente, retorne: ⁠"Classe x inserida com sucesso"
 Onde x é o valor da classe.*/
 
+function insertNewClass(string) {
+  const figure = {
+    name: "Carl",
+    classes: ["Warrior"],
+    leader_trend: true,
+    power: 6,
+    allied: true,
+  };
+  for (let i = 0; i < figure.classes.length; i++) {
+    if (figure.classes[i] > 2) {
+      return "Este personagem não pode estar em mais classes";
+    } else if (
+      string == "Warrior" ||
+      string == "Barbarian" ||
+      string == "Archer" ||
+      string == "Hunter" ||
+      string == "Survivor"
+    ) {
+      if (string == figure.classes[i]) {
+        return `O personagem já pertence a classe ${string}`;
+      } else {
+        return `Classe ${string} inserida com sucesso`;
+      }
+    }
+  }
+}
+insertNewClass("Hunter");
+console.log(insertNewClass());
+
 /*5- Escreva uma função chamada noMoreBarbarious. A função deve conseguir verificar se
 o objeto é um aliado.
 Todos que possuírem a classe "Barbarious" em sua lista serão considerado um inimigo.
@@ -81,3 +186,24 @@ Verifique se a propriedade allied está como verdadeira; se sim, verifique se a 
 troque a propriedade allied de verdadeira para falsa e retorne:
 ⁠"O x agora é um inimigo".
 Onde x é o nome do objeto.*/
+/* 
+function noMoreBarbarians() {
+  const figure = {
+    name: "Carl",
+    classes: ["Warrior", "Barbarian"],
+    leader_trend: true,
+    power: 6,
+    allied: true,
+  };
+  for (let i = 0; i < figure.classes.length; i++) {
+    if (figure.allied == true) {
+      if (figure.classes[i] == "Barbarian") {
+        allied = false;
+        return `O ${figure.name} agora é um inimigo`;
+      } 
+    } 
+  }
+}
+noMoreBarbarians();
+console.log(noMoreBarbarians());
+*/
