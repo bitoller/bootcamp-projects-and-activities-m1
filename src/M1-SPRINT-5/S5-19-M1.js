730811096bb744dfc2ab5bc6850b3cebcb41ddc5
bitoller <bi_toller@hotmail.com>
2022-12-19 18:06:20 -0300

    added the instructions

---
+++ src/M1-SPRINT-5/S5-19-M1.js
@@ -0,0 +1,69 @@
+/*1- Desenvolva uma função chamada vowelsCount que recebe um parâmetro:
+1. lista, um array de strings.
+Sua função deve retornar um novo array substituindo as
+posições das strings por um número representando a contagem de
+vogais daquelas palavras.*/
+
+
+
+/*2- Desenvolva uma função chamada stringTripletGroup que recebe um parâmetro:
+1. string, uma string representando uma frase.
+Sua função deve retornar um novo array com esta frase fatiada
+em 3 em 3 caracteres posicionados ao longo do array.
+Caso a quantidade de letras da última posição seja menor que 3,
+deverá ser completado com espaços vazios.*/
+
+
+
+/*3- Desenvolva uma função chamada dominantPet que recebe um parâmetro:
+1. lista, um array de strings contendo as palavras "dog" ou "cat"
+escritas sem um padrão definido.
+Sua função deve retornar uma string com o tipo de pet que aparece
+mais vezes no array, ou seja, o dominante em maiúsculo
+e com uma exclamação ("CAT!" ou "DOG!"). Caso a quantidade dos dois seja igual,
+deverá retornar a string "DRAW!".*/
+
+
+
+/*4- Desenvolva uma função chamada divisibleList que recebe dois parâmetros:
+1. lista, um array de números.
+2. numero, um número divisor.
+Sua função deve percorrer o array recebido por parâmetro,
+e retornar um novo array somente com os números divisíveis
+pelo parâmetro divisor.*/
+
+
+
+/*5- Desenvolva uma função chamada trustMeOrNot que recebe dois parâmetros:
+1. bool, um valor booleando true ou false
+1. numero, um número inteiro positivo.
+Sua função deve retornar um array com a quantidade de posições
+passadas por parâmetro e cada uma destas casas preenchido com o
+valor booleano escolhido pelo parâmetro.*/
+
+
+
+/*6- Desenvolva uma função chamada changeLampStatus que recebe um parâmetro:
+1. lista, um array com somente valores booleanos representando o
+estado da lâmpada (ligado/desligado)
+Sua função deve retornar um novo array com os valores booleanos
+de cada posição invertido.*/
+
+
+
+/*7- Desenvolva uma função chamada averageGrade que recebe um parâmetro:
+1. lista, um array com números que representam as notas de atividades de um aluno (0.0 até 10.0)
+Sua função deve retornar uma string dizendo qual a média destas notas.
+O valor da média deve se limitar a duas casas decimais.*/
+
+
+
+/*8- Desenvolva uma função chamada canvasDeliveriesPercentage que
+recebe um parâmetro:
+1. lista, um array com vários números inteiros/float representando
+as notas de um aluno (0.0 até 10.0).
+Sua função deve retornar uma string dizendo qual a porcentagem
+de entregas que o aluno atingiu.
+Consideraremos que cada entrega possui o valor de 10.0 pontos,
+sendo este o máximo.
+O valor da porcentagem deve se limitar a duas casas decimais.*/
