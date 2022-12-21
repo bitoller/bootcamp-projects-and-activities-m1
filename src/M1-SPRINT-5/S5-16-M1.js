1c01c9ac9609a703891d3b9a93d65a8e87ff779d
bitoller <bi_toller@hotmail.com>
2022-12-21 19:42:23 -0300

    solved

--- src/M1-SPRINT-5/S5-16-M1.js
+++ src/M1-SPRINT-5/S5-16-M1.js
@@ -19,7 +19,11 @@ A escola pode estar true(aberta) ou false(fechada).
 A função deve ser capaz de alternar o status de true para false e de
 false para true.*/
 
-
+function changeStatus(object) {
+  object.status = !object.status;
+  return object;
+}
+console.log(changeStatus(escola));
 
 /*2- Escreva uma função chamada alteraNivel que recebe como
 parâmetro um objeto escola e um valor para o nível.
@@ -30,21 +34,48 @@ Normalize as informações para realizar uma comparação precisa.
 A primeira letra da string, deve ficar sempre em maiúsculo e as
 demais em minúsculo. Retorne o objeto atualizado.*/
 
-
+function changeLevel(object, value) {
+  object.nivel = value.toLowerCase();
+  if (
+    object.nivel == "primário" ||
+    object.nivel == "fundamental" ||
+    object.nivel == "ensino médio"
+  ) {
+    object.nivel = object.nivel.charAt(0).toUpperCase() + object.nivel.slice(1);
+    return object;
+  } else {
+    return "Valor inválido";
+  }
+}
+console.log(changeLevel(escola, "fundamental"));
 
 /*3- Escreva uma função chamada alteraNome que recebe como parâmetro
 um objeto escola e um valor para o nome.
 Faça a validação do Nome, não pode ser inserido formatos diferentes de string,
 caso seja um formato diferente, retorne: 'Formato inválido'.*/
 
-
+function changeName(object, value) {
+  if (typeof value !== "string") {
+    return "Formato inválido";
+  }
+  object.nome = value;
+  return object;
+}
+console.log(changeName(escola, "Escola Estudo"));
 
 /*4- Escreva uma função chamada classificaEscola que recebe como
 parâmetro um objeto escola e um valor para a nota.
 Nota deve ser do tipo number e deve estar no intervalo de 1 a 5,
 caso contrário retorne: 'valor inválido'.*/
 
-
+function classifySchool(object, value) {
+  if (typeof value == "number" && value >= 1 && value <= 5) {
+    object.nota = value;
+    return object;
+  }
+  return "Valor Inválido";
+}
+console.log(classifySchool(escola, 5));
 
 /*5- Escreva uma função chamada verificaStatus que recebe como
 parâmetro um objeto escola.
@@ -52,7 +83,10 @@ Sua função deve verificar se o status da escola é true ou false.
 Se o status da for true, você deve retornar; 'Escola aberta',
 caso contrário retorne; 'Escola fechada'.*/
 
-
+function verifyStatus(object) {
+  return object.status ? "Escola aberta" : "Escola fechada";
+}
+console.log(verifyStatus(escola));
 
 /*6- Escreva uma função chamada verificaQualidadeEnsino que recebe
 como parâmetro um objeto escola.
@@ -61,4 +95,13 @@ Se a nota estiver entre 1 e 2, retorne: 'Esta não é uma boa escola'.
 Se estiver entre 3 e 4, retorne: 'Esta é uma boa escola' .
 Se a nota for 5, retorne: 'Uma escola excelente'*/
 
-
+function verifyTeachingQuality(object) {
+  if (object.nota >= 1 && object.nota <= 2) {
+    return "Esta não é uma boa escola";
+  } else if (object.nota >= 3 && object.nota <= 4) {
+    return "Esta é uma boa escola";
+  } else {
+    return "Uma escola excelente";
+  }
+}
+console.log(verifyTeachingQuality(escola));
