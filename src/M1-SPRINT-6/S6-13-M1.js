4378107cc357d93fd5d2b25fbc6934a106f28272
bitoller <bi_toller@hotmail.com>
2023-01-04 20:05:06 -0300

    done

---
+++ src/M1-SPRINT-6/S6-13-M1.js
@@ -0,0 +1,109 @@
+let items = [
+  {
+    image: "./assets/img/actions/animewoman.jpg",
+    name: "Anime Woman Figure",
+    price: "R$ 200,00",
+    type: "figure",
+  },
+
+  {
+    image: "./assets/img/actions/dragonballpersonagem.jpg",
+    name: "Dragonball Figure",
+    price: "R$ 250,00",
+    type: "figure",
+  },
+
+  {
+    image: "./assets/img/actions/starwarspersonagem.jpg",
+    name: "Star Wars Figure",
+    price: "R$ 260,00",
+    type: "figure",
+  },
+
+  {
+    image: "./assets/img/painting/clock.jpg",
+    name: "Apple Watch Painting",
+    price: "R$ 120,00",
+    type: "frame",
+  },
+
+  {
+    image: "./assets/img/painting/gamepad.jpg",
+    name: "PlayStation Controller Painting",
+    price: "R$ 120,00",
+    type: "frame",
+  },
+
+  {
+    image: "./assets/img/painting/personagem.jpg",
+    name: "Star Wars Painting",
+    price: "R$ 120,00",
+    type: "frame",
+  },
+
+  {
+    image: "./assets/img/actions/starwarspersonagem.jpg",
+    name: "Star Wars Figure",
+    price: "R$ 260,00",
+    type: "figure",
+  },
+
+  {
+    image: "./assets/img/painting/personagem.jpg",
+    name: "Star Wars Painting",
+    price: "R$ 120,00",
+    type: "frame",
+  },
+];
+
+let listFigures = [];
+let listFrames = [];
+
+function separateItens(list) {
+  for (let index = 0; index < list.length; index++) {
+    if (list[index].type == "frame") {
+      listFrames.push(list[index]);
+    } else {
+      listFigures.push(list[index]);
+    }
+  }
+}
+separateItens(items);
+console.log(listFrames);
+console.log(listFigures);
+
+let listPaintingSection =
+  document.getElementsByClassName("paintingsSection")[0];
+let listFigureSection = document.getElementsByClassName("figuresSection")[0];
+
+for (let index = 0; index < listFrames.length; index++) {
+  let elementList = document.createElement("li");
+  let imgCard = document.createElement("img");
+  imgCard.src = listFrames[index].image;
+  imgCard.alt = listFrames[index].name;
+  imgCard.classList.add("imgCard");
+  let name = document.createElement("span");
+  name.innerText = `${listFrames[index].name}`;
+  let p = document.createElement("p");
+  p.innerText = `${listFrames[index].price}`;
+  elementList.appendChild(imgCard);
+  elementList.appendChild(name);
+  elementList.appendChild(p);
+  listPaintingSection.appendChild(elementList);
+}
+
+for (let index = 0; index < listFigures.length; index++) {
+  let elementList = document.createElement("li");
+  let imgCard = document.createElement("img");
+  imgCard.src = listFigures[index].image;
+  imgCard.alt = listFigures[index].name;
+  imgCard.classList.add("imgCard");
+  let name = document.createElement("span");
+  name.innerText = `${listFigures[index].name}`;
+  let p = document.createElement("p");
+  p.innerText = `${listFigures[index].price}`;
+  elementList.appendChild(imgCard);
+  elementList.appendChild(name);
+  elementList.appendChild(p);
+  listFigureSection.appendChild(elementList);
+}
