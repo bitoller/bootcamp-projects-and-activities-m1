2a49771b6cf8b494effc2201934ae787f2173c43
bitoller <bi_toller@hotmail.com>
2023-01-10 12:39:24 -0300

    changes

---
+++ src/M1-SPRINT-7/S7-06-M1.js
@@ -0,0 +1,6 @@
+const chores = [
+  { title: "Pick up after the dog", type: "urgent" },
+  { title: "Do the dishes", type: "urgent" },
+  { title: "Dust the shelves off", type: "priority" },
+  { title: "Vacuum around", type: "normal" },
+];
