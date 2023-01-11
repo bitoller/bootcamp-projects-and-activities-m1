c4e821fd05f72f6fa398f9b8878e90be77a3b586
bitoller <bi_toller@hotmail.com>
2023-01-11 20:00:24 -0300

    added object

---
+++ src/M1-SPRINT-7/S7-13-M1.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    name: "Madalena",
+    profile: "Admin",
+    sector: "Administration",
+  },
+  {
+    name: "Rafael",
+    profile: "user",
+    sector: "Finance",
+  },
+  {
+    name: "Baltazar",
+    profile: "user",
+    sector: "Reception",
+  },
+  {
+    name: "Carmen",
+    profile: "Admin",
+    sector: "Business",
+  },
+];
