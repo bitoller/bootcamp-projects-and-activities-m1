3fe095303ca30db4b97c08a439e27b9185b2a055
bitoller <bi_toller@hotmail.com>
2023-01-13 12:08:07 -0300

    base of the final project done

---
+++ src/M1-SPRINT-8/S8-01-M1.js
@@ -0,0 +1,62 @@
+const data = [
+  {
+    id: 1,
+    img: "./img/jaqueta.svg",
+    nameItem: "Lightweight Jacket",
+    description:
+      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 2,
+    img: "./img/gorro.svg",
+    nameItem: "Black Hat",
+    description:
+      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 3,
+    img: "./img/mascara.svg",
+    nameItem: "Mask",
+    description:
+      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
+    value: 40,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Acessórios"],
+  },
+  {
+    id: 4,
+    img: "./img/camiseta_preta.svg",
+    nameItem: "T-Shirt",
+    description:
+      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 5,
+    img: "./img/camiseta_branca.svg",
+    nameItem: "Short-Sleeve T-Shirt",
+    description:
+      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+  {
+    id: 6,
+    img: "./img/moletom.svg",
+    nameItem: "Champion Packable Jacket",
+    description:
+      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
+    value: 100,
+    addCart: "Adicionar ao carrinho",
+    tag: ["Camisetas"],
+  },
+];
